4b3876c1c0bc2aeb0de427e3ecdf2387e6e1db82
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-15 19:42:03 +0700

    Update linuxhelper.py

--- userbot/modules/linuxhelper.py
+++ userbot/modules/linuxhelper.py
@@ -11,23 +11,23 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.lhelp$")
 async def usit(e):
     await e.edit(
-        f"**Hai Tuan {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Mastah](t.me/xflicks)"
-        "\n[Repo](https://github.com/ferikunn/Linux-Userbot)"
-        "\n[Instagram Mastah](Instagram.com/ferikunn)")
+        f"**Hai Bang {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        "\n[Abang²an](t.me/PashaDIE)"
+        "\n[Repo](https://github.com/PashaDIE/Last-UBOT)"
+        "\n[Instagram Abang²an](Instagram.com/pdie.09)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ferikunn/Linux-Userbot/Linux-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/PashaDIE/Last-UBOT/Last-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
-    "linuxhelper":
-    "🐧CMD🐧`.lhelp`\
+    "lasthelper":
+    "🐣CMD🐣`.lhelp`\
 \nPenjelasan: Bantuan Untuk saya-Userbot.\
-\n🐧CMD🐧`.vars`\
+\n🐣CMD🐣`.vars`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
