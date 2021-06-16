57dddc9aa15645e02030251be7cd487145c2d2a2
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-16 20:27:58 +0700

    Create salken.py

---
+++ userbot/modules/salken.py
@@ -0,0 +1,23 @@
+""" Userbot module salken for deafult user"""
+
+import asyncio
+
+from userbot import ALIVE_NAME, CMD_HELP, COUNTRY, USER_AGE
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.salken$")
+async def salken(salken):
+    """ Basically it's .salken command """
+    await salken.edit(f"`Heyyo bang Gua {ALIVE_NAME}`")
+    await asyncio.sleep(2)
+    await salken.edit(f"`Umur gua {USER_AGE}`")
+    await asyncio.sleep(2)
+    await salken.edit(f"`Gua tinggal di {COUNTRY} Salam Kenal ya bang:)`")
+
+
+CMD_HELP.update({
+    "salken":
+    "🐣 **Cmd** : `.salken`\
+    \n🐣 **Descriptions** : Command Salken Buat lu :)."
+})
