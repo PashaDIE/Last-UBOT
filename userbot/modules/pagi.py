ff76f01e94bdd51aa7608c7ae27b3a2c0f46ae70
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-17 22:09:09 +0700

    Create pagi.py

---
+++ userbot/modules/pagi.py
@@ -0,0 +1,27 @@
+from time import sleep
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@register(outgoing=True, pattern='^.pagi(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("▄▀▀──▄▀▀▄─▄▀▀▄─█▀▄")
+    sleep(1)
+    await typew.edit("█─▀█─█──█─█──█─█─█")
+    sleep(1)         
+    await typew.edit("─▀▀───▀▀───▀▀──▀▀─")
+    sleep(1)       
+    await typew.edit("█▄─▄█─▄▀▀▄─█▀▄─█▄─█─█─█▄─█─▄▀▀─")
+    sleep(1)                 
+    await typew.edit("█─▀─█─█──█─██▀─█─▀█─█─█─▀█─█─▀█")
+    sleep(1)               
+    await typew.edit("▀───▀──▀▀──▀─▀─▀──▀─▀─▀──▀──▀▀─")
+    sleep(1)
+    await typew.edit("𝐆𝐨𝐨𝐝 𝐦𝐨𝐫𝐧𝐢𝐧𝐠.🌄")
+    
+    CMD_HELP.update({
+    'Good morning':
+    '🐣CMD🐣`.pagi`\
+        \nUsage: Coba aja:).'
+})
