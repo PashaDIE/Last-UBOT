f8ffa113e44e9ffb15f6153289f97c7f1bc8e54c
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-17 18:28:01 +0700

    Create rasengan.py

---
+++ userbot/modules/rasengan.py
@@ -0,0 +1,23 @@
+from time import sleep
+from userbot.events import register
+from userbot CMD_HELP
+
+@register(outgoing=True, pattern='^.rasengan(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Hello")
+    sleep(2)
+    await typew.edit("Im Naruto Uzumaki And Im The Hokage Of Hidden Leaf Village!")
+    sleep(5)
+    await typew.edit("You Are Gonna Pay For What You Did!")
+    sleep(6)
+    await typew.edit("(Ξ｀Д´)🌀)))Rasengan！！")
+    sleep(2)
+    await typew.edit("You: ( ✖╭╮✖ )")
+# Create by myself @PashaDIE
+
+
+CMD_HELP.update({
+    "rasengan":
+    "🐣CMD🐣`.rasengan`\
+\nPenjelasan: Cek lah asw.\
