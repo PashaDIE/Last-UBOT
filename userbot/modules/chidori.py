960248a538b0d5d760cb413399c949a6917fc213
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-17 16:43:10 +0700

    Update chidori.py

--- userbot/modules/chidori.py
+++ userbot/modules/chidori.py
@@ -5,14 +5,14 @@ from userbot.events import register
 @register(outgoing=True, pattern='^.chidori(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
     await typew.edit("Hello")
-    sleep(5)
-    await typew.edit("Gua Kakashi Lu Mungkin Mengenal Gua Sebagai Copy Ninja")
-    sleep(10)
-    await typew.edit("Lu Akan Membayar Untuk Apa yang Lu Lakukan Pada Rekan-Rekan Gua")
     sleep(2)
+    await typew.edit("Gua Kakashi Lu Mungkin Mengenal Gua Sebagai Copy Ninja")
+    sleep(5)
+    await typew.edit("Lu Akan Membayar Untuk Apa yang Lu Lakukan Pada Rekan-Rekan Gua")
+    sleep(6)
     await typew.edit("( ◗_ ╂ ) ☞✹)Chidori ")
+    sleep(2)
     await typew.edit("You:(✖﹏✖)")
 
 # Create by myself @PashaDIE
