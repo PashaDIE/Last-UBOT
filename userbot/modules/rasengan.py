8d9545157155f35750ae94e3ef65a979f69e2f57
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-17 18:29:36 +0700

    Update rasengan.py

--- userbot/modules/rasengan.py
+++ userbot/modules/rasengan.py
@@ -1,6 +1,6 @@
 from time import sleep
 from userbot.events import register
-from userbot CMD_HELP
+from userbot import CMD_HELP
 
 @register(outgoing=True, pattern='^.rasengan(?: |$)(.*)')
 async def typewriter(typew):
