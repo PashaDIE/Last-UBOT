56d4fa51d45528c983301e7488ab2a9b46e2b990
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-17 18:24:02 +0700

    Update and rename naruto.py to Chidori.py

--- userbot/modules/Chidori.py
+++ userbot/modules/Chidori.py
@@ -16,19 +16,3 @@ async def typewriter(typew):
     await typew.edit("You:(✖﹏✖)")
 
 # Create by myself @PashaDIE
-
-
-@register(outgoing=True, pattern='^.rasengan(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("Hello")
-    sleep(2)
-    await typew.edit("Im Naruto Uzumaki And Im The Hokage Of Hidden Leaf Village!")
-    sleep(5)
-    await typew.edit("You Are Gonna Pay For What You Did!")
-    sleep(6)
-    await typew.edit("(Ξ｀Д´)🌀)))Rasengan！！")
-    sleep(2)
-    await typew.edit("You: ( ✖╭╮✖ )")
-
-# Create by myself @PashaDIE
