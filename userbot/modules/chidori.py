9946a18b39d163d8583dc325def36314633383dc
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-17 17:09:34 +0700

    Update chidori.py

--- userbot/modules/chidori.py
+++ userbot/modules/chidori.py
@@ -16,11 +16,31 @@ async def typewriter(typew):
     sleep(2)
     await typew.edit("You:(✖﹏✖)")
 
-CMD_HELP.update(
-    {
-        "babu": "🐣 **Cmd** : `.chidori`"
-        "\n🐣 **Descriptions** : Test aja Bang."
-    }
-)
-
 # Create by myself @PashaDIE
+
+from time import sleep
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@register(outgoing=True, pattern='^.rasengan(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Hello")
+    sleep(2)
+    await typew.edit("Im Naruto Uzumaki And Im The Hokage Of Hidden Leaf Village!")
+    sleep(5)
+    await typew.edit("You Are Gonna Pay For What You Did!")
+    sleep(6)
+    await typew.edit("(Ξ｀Д´)🌀)))Rasengan！！")
+    sleep(2)
+    await typew.edit("You: ( ✖╭╮✖ )")
+ 
+# Create by myself @PashaDIE
+
+CMD_HELP.update({
+    "naruto":
+    "🐣CMD🐣`.rasengan`\
+\nPenjelasan: Test aja bang.\
+\n\n🐣CMD🐣`.chidori`\
+\nPenjelasan: Test aja bang.\
