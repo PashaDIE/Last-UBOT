b1997a27f8c7382b9b6ec4d1208e696b92ba6753
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2021-06-18 10:59:26 +0700

    Create mikey.py

---
+++ userbot/modules/mikey.py
@@ -0,0 +1,69 @@
+#import userbot by pasha
+
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.wibbu(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MI MIII MII MIII.....**")
+    sleep(1)
+    await typew.edit("__PP MIKEY ANJIRRRRR!....__")
+    sleep(1)
+    await typew.edit("`🏃                        🏂`")
+    await typew.edit("`🏃                       🏂`")
+    await typew.edit("`🏃                      🏂`")
+    await typew.edit("`🏃                     🏂`")
+    await typew.edit("`🏃   `RUN`          🏂`")
+    await typew.edit("`🏃                   🏂`")
+    await typew.edit("`🏃                  🏂`")
+    await typew.edit("`🏃                 🏂`")
+    await typew.edit("`🏃                🏂`")
+    await typew.edit("`🏃               🏂`")
+    await typew.edit("`🏃              🏂`")
+    await typew.edit("`🏃             🏂`")
+    await typew.edit("`🏃            🏂`")
+    await typew.edit("`🏃           🏂`")
+    await typew.edit("`🏃..Tolong..🏂`")
+    await typew.edit("`🏃           🏂`")
+    await typew.edit("`🏃            🏂`")
+    await typew.edit("`🏃             🏂`")
+    await typew.edit("`🏃              🏂`")
+    await typew.edit("`🏃               🏂`")
+    await typew.edit("`🏃                🏂`")
+    await typew.edit("`🏃                 🏂`")
+    await typew.edit("`🏃                  🏂`")
+    await typew.edit("`🏃                   🏂`")
+    await typew.edit("`🏃                    🏂`")
+    await typew.edit("`🏃                     🏂`")
+    await typew.edit("`🏃 A.ada pp mikey       🏂`")
+    await typew.edit("`🏃                   🏂`")
+    await typew.edit("`🏃                  🏂`")
+    await typew.edit("`🏃                 🏂`")
+    await typew.edit("`🏃                🏂`")
+    await typew.edit("`🏃               🏂`")
+    await typew.edit("`🏃              🏂`")
+    await typew.edit("`🏃             🏂`")
+    await typew.edit("`🏃            🏂`")
+    await typew.edit("`🏃           🏂`")
+    await typew.edit("`🏃          🏂`")
+    await typew.edit("`🏃         🏂`")
+    await typew.edit("__KOK MAKIN DEKET SI PP MIKEY__")
+    sleep(1)
+    await typew.edit("`🏃       🏂`")
+    await typew.edit("`🏃      🏂`")
+    await typew.edit("`🏃     🏂`")
+    await typew.edit("`🏃    🏂`")
+    await typew.edit("**Untung Ga dikeluarin Jurus tendangan nuklirnya**")
+    sleep(1)
+    await typew.edit("🎯")
+    sleep(2)
+    await typew.edit("**Kasian Pemuja Mikey wkwk**")
+
+CMD_HELP.update({
+    'Mikey':
+    '🐣CMD🐣`.mikey`\
+        \nUsage: Buat pemuja mikey:).'
+})
